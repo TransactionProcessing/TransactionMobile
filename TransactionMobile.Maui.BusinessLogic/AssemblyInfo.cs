36468edf12485ff9de6feff1b19ed3488817df65
Stuart Ferguson <stuart_ferguson1@outlook.com>
2022-02-17 19:07:57 +0000

    tweaks

--- TransactionMobile.Maui.BusinessLogic/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using Microsoft.Maui.Controls;
-using TransactionMobile.Maui.BusinessLogic.ViewModels;
-
-[assembly: XmlnsDefinition("http://transactionmobile.maui.com/schemas/viewmodels", "TransactionMobile.Maui.BusinessLogic.ViewModels")]
-[assembly: XmlnsDefinition("http://transactionmobile.maui.com/schemas/transactionsviewmodels", "TransactionMobile.Maui.BusinessLogic.ViewModels.Transactions")] 
