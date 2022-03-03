8c4d041e22dfac347582f4b08f695cb55bb4d188
Stuart Ferguson <stuart_ferguson1@outlook.com>
2022-03-03 06:59:05 +0000

    Fix unit tests :|

--- TransactionMobile.Maui.BusinessLogic.Tests/RequestHandlerTests/LoginRequestHandlerTests.cs
+++ TransactionMobile.Maui.BusinessLogic.Tests/RequestHandlerTests/LoginRequestHandlerTests.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Models;
 using Moq;
 using RequestHandlers;
 using Requests;
@@ -17,12 +18,30 @@ public class LoginRequestHandlerTests
     {
         Mock<IAuthenticationService> authenticationService = new Mock<IAuthenticationService>();
         authenticationService.Setup(a => a.GetToken(It.IsAny<String>(), It.IsAny<String>(), It.IsAny<CancellationToken>())).ReturnsAsync(TestData.AccessToken);
-        LoginRequestHandler handler = new LoginRequestHandler(authenticationService.Object);
+        Mock<IConfigurationService> configurationService = new Mock<IConfigurationService>();
+        configurationService.Setup(c => c.GetConfiguration(It.IsAny<String>(), It.IsAny<CancellationToken>())).ReturnsAsync(new Configuration());
+        LoginRequestHandler handler = new LoginRequestHandler(authenticationService.Object,configurationService.Object);
             
         LoginRequest request = LoginRequest.Create(TestData.UserName,TestData.Password);
 
         String accessToken = await handler.Handle(request, CancellationToken.None);
 
         accessToken.ShouldBe(TestData.AccessToken);
+    }
+
+    [Fact]
+    public async Task LoginRequestHandler_Handle_GetConfigurationRequest_IsHandled()
+    {
+        Mock<IAuthenticationService> authenticationService = new Mock<IAuthenticationService>();
+        authenticationService.Setup(a => a.GetToken(It.IsAny<String>(), It.IsAny<String>(), It.IsAny<CancellationToken>())).ReturnsAsync(TestData.AccessToken);
+        Mock<IConfigurationService> configurationService = new Mock<IConfigurationService>();
+        configurationService.Setup(c => c.GetConfiguration(It.IsAny<String>(), It.IsAny<CancellationToken>())).ReturnsAsync(new Configuration());
+        LoginRequestHandler handler = new LoginRequestHandler(authenticationService.Object, configurationService.Object);
+
+        GetConfigurationRequest request = GetConfigurationRequest.Create(TestData.DeviceIdentifier);
+
+        Configuration configuration = await handler.Handle(request, CancellationToken.None);
+
+        configuration.ShouldNotBeNull();
     }
 }
