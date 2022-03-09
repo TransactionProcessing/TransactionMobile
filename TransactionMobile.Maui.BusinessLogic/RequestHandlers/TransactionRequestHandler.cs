f9ca79a4e4ae09712f4551e99f94b2b6a6ecab7c
Stuart Ferguson <stuart_ferguson1@outlook.com>
2022-03-09 08:39:42 +0000

    Cache and use transaction number

--- TransactionMobile.Maui.BusinessLogic/RequestHandlers/TransactionRequestHandler.cs
+++ TransactionMobile.Maui.BusinessLogic/RequestHandlers/TransactionRequestHandler.cs
@@ -1,5 +1,6 @@
 ﻿namespace TransactionMobile.Maui.BusinessLogic.RequestHandlers;
 
+using Database;
 using MediatR;
 using Models;
 using Requests;
@@ -13,13 +14,16 @@ public class TransactionRequestHandler : IRequestHandler<PerformMobileTopupReque
 
     private readonly ITransactionService TransactionService;
 
+    private readonly IDatabaseContext DatabaseContext;
+
     #endregion
 
     #region Constructors
 
-    public TransactionRequestHandler(ITransactionService transactionService)
+    public TransactionRequestHandler(ITransactionService transactionService, IDatabaseContext databaseContext)
     {
         this.TransactionService = transactionService;
+        this.DatabaseContext = databaseContext;
     }
 
     #endregion
@@ -29,6 +33,8 @@ public class TransactionRequestHandler : IRequestHandler<PerformMobileTopupReque
     public async Task<Boolean> Handle(PerformMobileTopupRequest request,
                                       CancellationToken cancellationToken)
     {
+        (TransactionRecord transactionRecord, Int64 transactionNumber) transaction = await this.CreateTransactionRecord(request, cancellationToken);
+
         // TODO: Factory
         PerformMobileTopupRequestModel model = new PerformMobileTopupRequestModel
                                                {
@@ -46,22 +52,107 @@ public class TransactionRequestHandler : IRequestHandler<PerformMobileTopupReque
 
         Boolean result = await this.TransactionService.PerformMobileTopup(model, cancellationToken);
 
+        await this.UpdateTransactionRecord(transaction.transactionRecord, result, cancellationToken);
+
         return result;
+    }
+
+    private async Task<(TransactionRecord transactionRecord, Int64 transactionNumber)> CreateTransactionRecord(LogonTransactionRequest request,
+                                                                                                               CancellationToken cancellationToken)
+    {
+        TransactionRecord transactionRecord = new TransactionRecord
+                                              {
+                                                  ApplicationVersion = request.ApplicationVersion,
+                                                  DeviceIdentifier = request.DeviceIdentifier,
+                                                  TransactionDateTime = request.TransactionDateTime,
+                                                  TransactionType = 1
+                                              };
+        Int64 transactionNumber = await this.DatabaseContext.CreateTransaction(transactionRecord);
+
+        return (transactionRecord, transactionNumber);
+    }
+
+    private async Task<(TransactionRecord transactionRecord, Int64 transactionNumber)> CreateTransactionRecord(PerformMobileTopupRequest request,
+                                                                                                               CancellationToken cancellationToken)
+    {
+        TransactionRecord transactionRecord = new TransactionRecord
+                                              {
+                                                  ApplicationVersion = request.ApplicationVersion,
+                                                  DeviceIdentifier = request.DeviceIdentifier,
+                                                  TransactionDateTime = request.TransactionDateTime,
+                                                  TransactionType = 2,
+                                                  Amount = request.TopupAmount,
+                                                  ProductId = request.ProductId,
+                                                  ContractId = request.ContractId,
+                                                  CustomerAccountNumber = request.CustomerAccountNumber,
+                                                  CustomerEmailAddress = request.CustomerEmailAddress,
+                                                  OperatorIdentifier = request.OperatorIdentifier
+                                              };
+        Int64 transactionNumber = await this.DatabaseContext.CreateTransaction(transactionRecord);
+
+        return (transactionRecord, transactionNumber);
+    }
+
+    private async Task<(TransactionRecord transactionRecord, Int64 transactionNumber)> CreateTransactionRecord(PerformVoucherIssueRequest request,
+                                                                                                               CancellationToken cancellationToken)
+    {
+        TransactionRecord transactionRecord = new TransactionRecord
+                                              {
+                                                  ApplicationVersion = request.ApplicationVersion,
+                                                  DeviceIdentifier = request.DeviceIdentifier,
+                                                  TransactionDateTime = request.TransactionDateTime,
+                                                  TransactionType = 2,
+                                                  Amount = request.VoucherAmount,
+                                                  ProductId = request.ProductId,
+                                                  ContractId = request.ContractId,
+                                                  RecipientEmailAddress = request.RecipientEmailAddress,
+                                                  RecipientMobileNumber = request.RecipientMobileNumber,
+                                                  CustomerEmailAddress = request.CustomerEmailAddress,
+                                                  OperatorIdentifier = request.OperatorIdentifier
+                                              };
+        Int64 transactionNumber = await this.DatabaseContext.CreateTransaction(transactionRecord);
+
+        return (transactionRecord, transactionNumber);
+    }
+
+    private async Task UpdateTransactionRecord(TransactionRecord transactionRecord,
+                                               PerformLogonResponseModel result,
+                                               CancellationToken cancellationToken)
+    {
+        transactionRecord.IsSuccessful = result.IsSuccessful;
+        transactionRecord.ResponseMessage = result.ResponseMessage;
+        transactionRecord.EstateId = result.EstateId;
+        transactionRecord.MerchantId = result.MerchantId;
+
+        await this.DatabaseContext.UpdateTransaction(transactionRecord);
+    }
+
+    private async Task UpdateTransactionRecord(TransactionRecord transactionRecord,
+                                               Boolean result,
+                                               CancellationToken cancellationToken)
+    {
+        transactionRecord.IsSuccessful = result;
+        
+        await this.DatabaseContext.UpdateTransaction(transactionRecord);
     }
 
     public async Task<PerformLogonResponseModel> Handle(LogonTransactionRequest request,
                                                         CancellationToken cancellationToken)
     {
+        (TransactionRecord transactionRecord, Int64 transactionNumber) transaction = await this.CreateTransactionRecord(request, cancellationToken);
+
         // TODO: Factory
         PerformLogonRequestModel model = new PerformLogonRequestModel
                                          {
                                              ApplicationVersion = request.ApplicationVersion,
                                              DeviceIdentifier = request.DeviceIdentifier,
                                              TransactionDateTime = request.TransactionDateTime,
-                                             TransactionNumber = request.TransactionNumber
-                                         };
-
+                                             TransactionNumber = transaction.transactionNumber.ToString()
+        };
+        
         PerformLogonResponseModel result = await this.TransactionService.PerformLogon(model, cancellationToken);
+
+        await this.UpdateTransactionRecord(transaction.transactionRecord, result, cancellationToken);
 
         return result;
     }
@@ -71,6 +162,7 @@ public class TransactionRequestHandler : IRequestHandler<PerformMobileTopupReque
     public async Task<Boolean> Handle(PerformVoucherIssueRequest request,
                                       CancellationToken cancellationToken)
     {
+        (TransactionRecord transactionRecord, Int64 transactionNumber) transaction = await this.CreateTransactionRecord(request, cancellationToken);
         // TODO: Factory
         PerformVoucherIssueRequestModel model = new PerformVoucherIssueRequestModel
         {
@@ -84,10 +176,12 @@ public class TransactionRequestHandler : IRequestHandler<PerformMobileTopupReque
                                                    ProductId = request.ProductId,
                                                    VoucherAmount = request.VoucherAmount,
                                                    TransactionDateTime = request.TransactionDateTime,
-                                                   TransactionNumber = request.TransactionNumber
-                                               };
+                                                   TransactionNumber = transaction.transactionNumber.ToString()
+        };
 
         Boolean result = await this.TransactionService.PerformVoucherIssue(model, cancellationToken);
+
+        await this.UpdateTransactionRecord(transaction.transactionRecord, result, cancellationToken);
 
         return result;
     }
---
+++ TransactionMobile.Maui.Database/IDatabaseContext.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionMobile.Maui.Database
+{
+    using SQLite;
+
+    public interface IDatabaseContext
+    {
+        Task InitialiseDatabase();
+
+        Task<Int64> CreateTransaction(TransactionRecord transactionRecord);
+
+        Task UpdateTransaction(TransactionRecord transactionRecord);
+
+        Task<List<TransactionRecord>> GetTransactions();
+    }
+
+    public class DatabaseContext : IDatabaseContext
+    {
+        private readonly SQLiteConnection Connection;
+
+        public DatabaseContext(String connectionString)
+        {
+            this.Connection = new SQLiteConnection(connectionString);
+        }
+
+        public async Task InitialiseDatabase()
+        {
+            this.Connection.CreateTable<TransactionRecord>();
+        }
+
+        public async Task<Int64> CreateTransaction(TransactionRecord transactionRecord)
+        {
+            this.Connection.Insert(transactionRecord);
+
+            return SQLite3.LastInsertRowid(this.Connection.Handle);
+        }
+
+        public async Task UpdateTransaction(TransactionRecord transactionRecord)
+        {
+            this.Connection.Update(transactionRecord);
+        }
+
+        public async Task<List<TransactionRecord>> GetTransactions()
+        {
+            return this.Connection.Table<TransactionRecord>().ToList();
+        }
+    }
+
+    public class TransactionRecord
+    {
+        public String ApplicationVersion { get; set; }
+        
+        public Guid ContractId { get; set; }
+
+        public String CustomerAccountNumber { get; set; }
+
+        public String CustomerEmailAddress { get; set; }
+
+        public String DeviceIdentifier { get; set; }
+
+        public String OperatorIdentifier { get; set; }
+
+        public Guid ProductId { get; set; }
+
+        public Decimal Amount { get; set; }
+
+        public DateTime TransactionDateTime { get; set; }
+
+        [PrimaryKey, AutoIncrement]
+        public Int32 TransactionNumber { get; set; }
+
+        public String RecipientMobileNumber { get; set; }
+        public String RecipientEmailAddress { get; set; }
+
+        public Int32 TransactionType { get; set; }
+
+        public Guid EstateId { get; set; }
+        public Guid MerchantId { get; set; }
+        public Boolean IsSuccessful { get; set; }
+
+        public String ResponseMessage { get; set; }
+
+    }
+}
