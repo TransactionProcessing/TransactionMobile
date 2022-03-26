7ce313d3f283bb1ff14af17270d1caab21801032
Stuart Ferguson <stuart_ferguson1@outlook.com>
2022-03-26 07:43:25 +0000

    :|

---
+++ TransactionMobile.Maui.BusinessLogic/Database/IDatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionMobile.Maui.Database
+{
+    public interface IDatabaseContext
+    {
+        Task InitialiseDatabase();
+
+        Task<Int64> CreateTransaction(TransactionRecord transactionRecord);
+
+        Task UpdateTransaction(TransactionRecord transactionRecord);
+
+        Task<List<TransactionRecord>> GetTransactions();
+        Task ClearStoredTransactions(List<TransactionRecord> transactionRecords);
+
+        Task<List<LogMessage>> GetLogMessages(Int32 batchSize);
+
+        Task InsertLogMessage(LogMessage logMessage);
+
+        Task InsertLogMessages(List<LogMessage> logMessages);
+
+        Task RemoveUploadedMessages(List<LogMessage> logMessagesToRemove);
+    }
+}
