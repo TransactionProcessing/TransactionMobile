730c3dc6206723e84296ce464bd5c40a0e85fa5d
Stuart Ferguson <stuart_ferguson1@outlook.com>
2022-05-18 11:09:31 +0100

    .

--- TransactionMobile.Maui.UiTests/Drivers/AppiumDriver.cs
+++ TransactionMobile.Maui.UiTests/Drivers/AppiumDriver.cs
@@ -62,7 +62,7 @@ namespace TransactionMobile.Maui.UiTests.Drivers
             driverOptions.DeviceName = "iPhone 11";
             
             String assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            String binariesFolder = Path.Combine(assemblyFolder, "..", "..", "..", "..", @"TransactionMobile.Maui/bin/Release/net6.0-ios/iossimulator-x64");
+            String binariesFolder = Path.Combine(assemblyFolder, "..", "..", "..", "..", @"TransactionMobile.Maui/bin/Release/net6.0-ios/iossimulator-x64/");
             var apkPath = Path.Combine(binariesFolder, "TransactionMobile.Maui.app");
             driverOptions.App = apkPath;
             driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.FullReset, true);
