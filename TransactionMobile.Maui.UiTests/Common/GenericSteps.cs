088e2566c55dbafc6ee3411754c1d6145a1ee657
Stuart Ferguson <stuart_ferguson1@outlook.com>
2022-12-22 17:55:57 +0000

    remove docker swtup for now

--- TransactionMobile.Maui.UiTests/Common/GenericSteps.cs
+++ TransactionMobile.Maui.UiTests/Common/GenericSteps.cs
@@ -25,7 +25,7 @@ public class GenericSteps
     [BeforeScenario(Order = 0)]
     public async Task StartSystem()
     {
-        Setup.GlobalSetup();
+        //Setup.GlobalSetup();
 
         // Initialise a logger
         String scenarioName = this.ScenarioContext.ScenarioInfo.Title.Replace(" ", "");
@@ -35,23 +35,23 @@ public class GenericSteps
 
         this.TestingContext.DockerHelper = new DockerHelper();
         this.TestingContext.DockerHelper.Logger = logger;
-        this.TestingContext.DockerHelper.SqlServerContainer = Setup.DatabaseServerContainer;
-        this.TestingContext.DockerHelper.SqlServerNetwork = Setup.DatabaseServerNetwork;
-        this.TestingContext.DockerHelper.DockerCredentials = Setup.DockerCredentials;
-        this.TestingContext.DockerHelper.SqlCredentials = Setup.SqlCredentials;
-        this.TestingContext.DockerHelper.SqlServerContainerName = "sharedsqlserver";
+        //this.TestingContext.DockerHelper.SqlServerContainer = Setup.DatabaseServerContainer;
+        //this.TestingContext.DockerHelper.SqlServerNetwork = Setup.DatabaseServerNetwork;
+        //this.TestingContext.DockerHelper.DockerCredentials = Setup.DockerCredentials;
+        //this.TestingContext.DockerHelper.SqlCredentials = Setup.SqlCredentials;
+        //this.TestingContext.DockerHelper.SqlServerContainerName = "sharedsqlserver";
 
         this.TestingContext.Logger = logger;
-        this.TestingContext.Logger.LogInformation("About to Start Containers for Scenario Run");
-        await this.TestingContext.DockerHelper.StartContainersForScenarioRun(scenarioName).ConfigureAwait(false);
-        this.TestingContext.Logger.LogInformation("Containers for Scenario Run Started");
+        //this.TestingContext.Logger.LogInformation("About to Start Containers for Scenario Run");
+        //await this.TestingContext.DockerHelper.StartContainersForScenarioRun(scenarioName).ConfigureAwait(false);
+        //this.TestingContext.Logger.LogInformation("Containers for Scenario Run Started");
     }
 
     [AfterScenario(Order = 0)]
     public async Task StopSystem()
     {
-        this.TestingContext.Logger.LogInformation("About to Stop Containers for Scenario Run");
-        await this.TestingContext.DockerHelper.StopContainersForScenarioRun().ConfigureAwait(false);
-        this.TestingContext.Logger.LogInformation("Containers for Scenario Run Stopped");
+        //this.TestingContext.Logger.LogInformation("About to Stop Containers for Scenario Run");
+        //await this.TestingContext.DockerHelper.StopContainersForScenarioRun().ConfigureAwait(false);
+        //this.TestingContext.Logger.LogInformation("Containers for Scenario Run Stopped");
     }
 }
