16e165f0301b1dd74696060fe61d8e9a54654444
Stuart Ferguson <stuart_ferguson1@outlook.com>
2024-03-06 20:31:28 +0000

    :|

--- TransactionMobile.Maui.UiTests/Drivers/AppiumDriver.cs
+++ TransactionMobile.Maui.UiTests/Drivers/AppiumDriver.cs
@@ -61,7 +61,7 @@ namespace TransactionMobile.Maui.UiTests.Drivers
             driverOptions.DeviceName = "WindowsPC";
             
             driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.FullReset, true);
-            driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.NewCommandTimeout, 600);
+            driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.NewCommandTimeout, 6000);
             
             driverOptions.App = "TransactionMobile_zct748q4xfh0m!App";
             AppiumDriverWrapper.Driver = new WindowsDriver(appiumService, driverOptions, TimeSpan.FromMinutes(10));
@@ -78,7 +78,7 @@ namespace TransactionMobile.Maui.UiTests.Drivers
             String binariesFolder = Path.Combine(assemblyFolder, "..", "..", "..", "..", @"TransactionMobile.Maui/bin/Release/net7.0-ios/iossimulator-x64/");
             var apkPath = Path.Combine(binariesFolder, "TransactionMobile.Maui.app");
             driverOptions.App = apkPath;
-            driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.NewCommandTimeout, 600);
+            driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.NewCommandTimeout, 6000);
             //driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.FullReset, true);
             //driverOptions.AddAdditionalAppiumOption("useNewWDA", true);
             //driverOptions.AddAdditionalAppiumOption("wdaLaunchTimeout", 999999999);
@@ -103,7 +103,7 @@ namespace TransactionMobile.Maui.UiTests.Drivers
             driverOptions.AddAdditionalAppiumOption("appPackage", "com.transactionprocessing.pos");
             driverOptions.AddAdditionalAppiumOption("enforceAppInstall", true);
             driverOptions.AddAdditionalAppiumOption("uiautomator2ServerInstallTimeout", "40000");
-            driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.NewCommandTimeout, 600);
+            driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.NewCommandTimeout, 6000);
 
             String assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             String binariesFolder = Path.Combine(assemblyFolder, "..", "..", "..", "..", @"TransactionMobile.Maui/bin/Release/net7.0-android/");
