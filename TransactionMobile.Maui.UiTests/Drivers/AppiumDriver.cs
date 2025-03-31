9c15ef4513b664f627c81faa6402fce34b675870
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2025-03-31 15:08:41 +0100

    :|

--- TransactionMobile.Maui.UiTests/Drivers/AppiumDriver.cs
+++ TransactionMobile.Maui.UiTests/Drivers/AppiumDriver.cs
@@ -75,15 +75,22 @@ namespace TransactionMobile.Maui.UiTests.Drivers
             driverOptions.PlatformName = "iOS";
             driverOptions.AddAdditionalAppiumOption("udid", Environment.GetEnvironmentVariable("UDID")); // Corrected capability.
 
+            //String assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            //String binariesFolder = Path.Combine(assemblyFolder, "..", "..", "..", "..", @"TransactionMobile.Maui/bin/Release/net8.0-ios/iossimulator-x64/");
+            //var apkPath = Path.Combine(binariesFolder, "TransactionMobile.Maui.app");
+
             String assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            String binariesFolder = Path.Combine(assemblyFolder, "..", "..", "..", "..", @"TransactionMobile.Maui/bin/Release/net8.0-ios/iossimulator-x64/");
-            var apkPath = Path.Combine(binariesFolder, "TransactionMobile.Maui.app");
-            driverOptions.App = apkPath;
-            driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.NewCommandTimeout, 6000);
+            String appPath = Path.Combine(assemblyFolder, "..", "..", "..", "..", @"TransactionMobile.Maui/bin/Release/net8.0-ios/iossimulator-x64/", "TransactionMobile.Maui.app");
+            driverOptions.App = appPath;
+            var fileInfo = new FileInfo(appPath);
+            Debug.WriteLine($"App File Exists {fileInfo.Exists}");
+
+            //driverOptions.App = apkPath;
+            //driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.NewCommandTimeout, 6000);
             //driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.FullReset, true);
-            driverOptions.AddAdditionalAppiumOption("useNewWDA", true);
-            driverOptions.AddAdditionalAppiumOption("wdaLaunchTimeout", 999999999);
-            driverOptions.AddAdditionalAppiumOption("wdaConnectionTimeout", 999999999);
+            //driverOptions.AddAdditionalAppiumOption("useNewWDA", true);
+            //driverOptions.AddAdditionalAppiumOption("wdaLaunchTimeout", 999999999);
+            //driverOptions.AddAdditionalAppiumOption("wdaConnectionTimeout", 999999999);
             //driverOptions.AddAdditionalAppiumOption("restart", true);
             //driverOptions.AddAdditionalAppiumOption("simulatorStartupTimeout", 5 * 60 * 1000);
             //driverOptions.AddAdditionalAppiumOption("wdaLocalPort", 8101); // Example: Change to port 8101
