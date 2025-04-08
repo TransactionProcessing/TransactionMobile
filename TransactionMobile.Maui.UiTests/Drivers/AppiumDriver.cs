437a06c70ab060f2b1ccec632d9338f5ec74b94b
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2025-04-08 15:10:33 +0100

    Add shouldWaitForQuiescence option to AppiumDriver
    Introduced a new Appium option `shouldWaitForQuiescence` set to `false` in the `AppiumDriver.cs` file. This option controls whether the driver waits for the application to become idle before executing further commands.

--- TransactionMobile.Maui.UiTests/Drivers/AppiumDriver.cs
+++ TransactionMobile.Maui.UiTests/Drivers/AppiumDriver.cs
@@ -83,6 +83,7 @@ namespace TransactionMobile.Maui.UiTests.Drivers
             driverOptions.App = apkPath;
             driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.NewCommandTimeout, 6000);
             driverOptions.AddAdditionalAppiumOption("waitForQuiescence", false);
+            driverOptions.AddAdditionalAppiumOption("shouldWaitForQuiescence", false);
             //driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.FullReset, true);
             //driverOptions.AddAdditionalAppiumOption("useNewWDA", true);
             //driverOptions.AddAdditionalAppiumOption("wdaLaunchTimeout", 999999999);
