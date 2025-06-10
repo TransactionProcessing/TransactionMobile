8295f9c4c43525f70827089ddb93c4b4eb484f04
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2025-06-10 06:52:47 +0100

    Update Android system image and package versions
    - Changed Android system image installation in `android_e2e_tests.yml` from version 34 to 35.
    - Updated `SecurityService.Client` to version `2025.5.2-build82` and `TransactionProcessorACL.DataTransferObjects` to `2025.3.2-build64` in `TransactionMobile.Maui.BusinessLogic.csproj`.
    - Modified Android platform version from `14.0` to `15.0` in `AppiumDriver.cs`.
    - Updated multiple package versions in `TransactionMobile.Maui.UiTests.csproj` to `2025.5.2-build186` and `2025.5.2-build82`.

--- TransactionMobile.Maui.UiTests/Drivers/AppiumDriver.cs
+++ TransactionMobile.Maui.UiTests/Drivers/AppiumDriver.cs
@@ -99,7 +99,7 @@ namespace TransactionMobile.Maui.UiTests.Drivers
             driverOptions.AddAdditionalAppiumOption("adbExecTimeout", TimeSpan.FromMinutes(5).TotalMilliseconds);
             driverOptions.AutomationName = "UIAutomator2";
             driverOptions.PlatformName = "Android";
-            driverOptions.PlatformVersion = "14.0";
+            driverOptions.PlatformVersion = "15.0";
             driverOptions.DeviceName = "emulator-5554";
             
             // TODO: Only do this locally
