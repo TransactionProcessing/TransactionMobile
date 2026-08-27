679c4dac227065d10c1ae4d00ac0a96970aaa47e
StuartFerguson <stuart_ferguson1development@outlook.com>
2026-08-27 10:27:39 +0100

    Convert metadata fields to static properties

--- TransactionProcessor.Mobile.BusinessLogic/Common/Extensions.MetadataFields.cs
+++ TransactionProcessor.Mobile.BusinessLogic/Common/Extensions.MetadataFields.cs
@@ -2,13 +2,13 @@ namespace TransactionProcessor.Mobile.BusinessLogic.Common;
 
 public static class MetadataFields
 {
-    public const string Amount = "Amount";
-    public const string RecipientEmail = "RecipientEmail";
-    public const string RecipientMobile = "RecipientMobile";
-    public const string CustomerAccountNumber = "CustomerAccountNumber";
-    public const string PataPawaPostPaidMessageType = "PataPawaPostPaidMessageType";
-    public const string PataPawaPrePayMessageType = "PataPawaPrePayMessageType";
-    public const string MeterNumber = "MeterNumber";
-    public const string CustomerName = "CustomerName";
-    public const string MobileNumber = "MobileNumber";
+    public static string Amount { get; } = "Amount";
+    public static string RecipientEmail { get; } = "RecipientEmail";
+    public static string RecipientMobile { get; } = "RecipientMobile";
+    public static string CustomerAccountNumber { get; } = "CustomerAccountNumber";
+    public static string PataPawaPostPaidMessageType { get; } = "PataPawaPostPaidMessageType";
+    public static string PataPawaPrePayMessageType { get; } = "PataPawaPrePayMessageType";
+    public static string MeterNumber { get; } = "MeterNumber";
+    public static string CustomerName { get; } = "CustomerName";
+    public static string MobileNumber { get; } = "MobileNumber";
 }
